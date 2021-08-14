9e0aeeb8cf1a4806c696ba9b3334c490220a6bbc
Chenwi <chenwieugene.j@gmail.com>
2021-08-14 11:47:20 +0100

    Eugene

--- js/man.js
+++ js/man.js
@@ -1,5 +1,29 @@
 const menuBtn = document.getElementById("btn");
-const menu = document.getElementsByClassName("mobile-menu");
+const menu = document.querySelector(".mobile-menu");
+const overlay = document.querySelector(".overlay");
 
-console.log(menu);
-menuBtn.addEventListener("click", () => {});
+console.log(menuBtn);
+menuBtn.addEventListener("click", () => {
+  if (menu.classList.contains("hide")) {
+    show();
+  } else {
+    hide();
+  }
+});
+
+overlay.addEventListener("click", () => {
+  if (menu.classList.contains("hide")) {
+    show();
+  } else {
+    hide();
+  }
+});
+
+const show = () => {
+  menu.classList.remove("hide");
+  overlay.classList.remove("hide-2");
+};
+const hide = () => {
+  menu.classList.add("hide");
+  overlay.classList.add("hide-2");
+};
